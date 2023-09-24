5457c6a942a885a811e4435df7307997a65a5a08
JinweiOS <jinwei.peng@beingthink.com>
2023-09-24 13:23:29 +0800

    feat: finish pnpm download

--- src/server/server.go
+++ src/server/server.go
@@ -1,10 +1,13 @@
 package server
 
 import (
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+
 	"github.com/gorilla/mux"
 )
-
 
 var version string
 var arch string
@@ -14,13 +17,27 @@ var router *mux.Router
 func init() {
 	router = mux.NewRouter()
 	prefixRouter := router.PathPrefix("/pnpm/pnpm/releases/download/").Subrouter()
-	prefixRouter.HandleFunc("/{version}/{arch}", func (w http.ResponseWriter, r *http.Request) {
+	prefixRouter.HandleFunc("/{version}/{arch}", func(w http.ResponseWriter, r *http.Request) {
 		params := mux.Vars(r)
 		version = params["version"]
 		arch = params["arch"]
 		println(version, arch)
+		// TODO: 向客户端发送文件
+		sendPnpm(version, arch, w)
 	})
-	// TODO: 向客户端发送文件
+}
+
+func sendPnpm(version string, arch string, w http.ResponseWriter) {
+	wkdir, _ := os.Getwd()
+	pnpmPath := filepath.Join(wkdir, "mirror", version, arch)
+	pnpmFile, err := os.Open(pnpmPath)
+	if err != nil {
+		println(err)
+	}
+	w.Header().Set("Content-Type", "application/octet-stream")
+	_, err = io.Copy(w, pnpmFile)
+	defer pnpmFile.Close()
+	println(pnpmPath)
 }
 
 func StartServer() {
